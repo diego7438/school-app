9617922dc2f96d42c9059ba08e93c2c7fc97e1c5
diego7438 <danderson26@windwardschool.org>
2025-12-27 22:34:11 -0800

    Fix timezone issue: Force dashboard to use LA time

--- my_windward_app/dashboard.py
+++ my_windward_app/dashboard.py
@@ -3,6 +3,7 @@ from flask import (
 )
 
 import datetime as dt
+import pytz
 from .rotation import get_rotation_for_date
 from .auth import login_required
 
@@ -11,8 +12,9 @@ bp = Blueprint('dashboard', __name__, url_prefix= '/dashboard')
 @bp.route('/')
 @login_required
 def index():
-    # Get today's date
-    today = dt.date.today()
+    # Get today's date in Pacific Time (Windward School location)
+    tz = pytz.timezone('America/Los_Angeles')
+    today = dt.datetime.now(tz).date()
 
     # Get the rotation info: returns (rotation_number, day_name) or None
     rotation_info = get_rotation_for_date(today.month, today.day)
